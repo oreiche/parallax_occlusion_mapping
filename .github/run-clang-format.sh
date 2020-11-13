514b39a4b86abc774d4dc363f7dcbbe60e2aab0a
Oliver Reiche <oliver.reiche@gmail.com>
2020-11-13 19:00:06 +0100

    Initial commit for C++

---
+++ .github/run-clang-format.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export PATH=".toolchain/bin:$PATH"
+
+readonly STORAGE_DIR="${TMPDIR:-/tmp}/run-clang-format_$USER"
+readonly EXEC_DIR="$STORAGE_DIR/exec.$$"
+
+function usage() {
+  echo "Usage: $(basename $0) [SRC_DIR] [OPTION]..."
+  echo "Run clang-format on every C/C++ file in SRC_DIR."
+  echo
+  echo "Possible OPTIONs are:"
+  echo "  -e REGEX  exclude regex pattern"
+  echo "  -f        fix formats of all files"
+  echo "  -v        verbose, print format diff"
+}
+
+function run_clang_format() {
+  local SRC_DIR="$(pwd)"
+  local EXCL_REGX=
+  local FIX_FORMAT=false
+  local VERBOSE=false
+
+  # parse source directory argument
+  if [ -n "$1" ] && ! [[ "$1" =~ ^- ]]; then
+    SRC_DIR="$1"
+    shift
+  fi
+
+  # parse remaining arguments
+  while getopts "h?e:fv" OPT "$@"; do
+    case "${OPT}" in
+      h|\?)
+        usage
+        exit 0
+        ;;
+      e)
+        EXCL_REGX="${OPTARG}"
+        ;;
+      f)
+        FIX_FORMAT=true
+        ;;
+      v)
+        VERBOSE=true
+        ;;
+    esac
+  done
+
+  mkdir -p "$EXEC_DIR"
+
+  printf "Running clang-format on %s\n" "$SRC_DIR"
+
+  local RETVAL=0 SRC_FILE
+  while read -r SRC_FILE; do
+    # skip non-C/C++ files
+    if ! file "$SRC_FILE" | grep -iq "c source\|c++ source"; then continue; fi
+
+    # skip on exclude regex match
+    if [ -n "$EXCL_REGX" ] && [[ "$SRC_FILE" =~ $EXCL_REGX ]]; then continue; fi
+
+    if $FIX_FORMAT; then
+      # fix format in-place
+      clang-format -verbose -i -style=file "$SRC_FILE"
+    else
+      # count format errors
+      local FMT_ERRS=$(clang-format -style=file \
+                                    -output-replacements-xml \
+                                    "$SRC_FILE" \
+                        | grep -c "replacement offset")
+      if [ "$FMT_ERRS" -gt "0" ]; then
+        RETVAL=1
+        printf "%4d format error(s) in %s\n" "$FMT_ERRS" "$SRC_FILE"
+        if ${VERBOSE}; then
+          local TMP_FILE="$EXEC_DIR/$(basename "$SRC_FILE")"
+          clang-format -style=file "$SRC_FILE" > "$TMP_FILE"
+          diff --color -u -p "$SRC_FILE" "$TMP_FILE" || true
+        fi
+      fi
+    fi
+  done < <(find "${SRC_DIR}" -type f)
+
+  # cleanup
+  rm -rf "$EXEC_DIR"
+
+  return $RETVAL
+}
+
+if [[ "$0" == "$BASH_SOURCE" ]]; then
+  run_clang_format "$@"
+  exit $?
+fi
---
+++ .github/run-clang-tidy.sh
@@ -0,0 +1,206 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export PATH=".toolchain/bin:$PATH"
+
+readonly CC_FLAGS=(
+  "-std=c++17"
+  "-I."
+)
+
+readonly STORAGE_DIR="${TMPDIR:-/tmp}/run-clang-tidy_$USER"
+readonly CACHE_DIR="$STORAGE_DIR/cache"
+readonly EXEC_DIR="$STORAGE_DIR/exec.$$"
+readonly LOG_DIR="$EXEC_DIR/logs"
+readonly FAIL_FLAG="$EXEC_DIR/failed"
+
+function usage() {
+  echo "Usage: $(basename $0) [SRC_DIR] [OPTION]..."
+  echo "Run clang-tidy on every C/C++ file in SRC_DIR."
+  echo
+  echo "Possible OPTIONs are:"
+  echo "  -e REGEX  exclude regex pattern"
+  echo "  -j N      jobs to run in parallel"
+  echo "  -n        no cache from previous runs"
+  echo "  -s        stop and exit on first error"
+  echo "  -v        verbose, print errors and warnings"
+}
+
+function job_limit_reached() {
+  local JOB_LIMIT=$1
+  shift
+  local INPUT_PIDS=("$@")
+  local COUNT=0
+  for PID in "${INPUT_PIDS[@]}"; do
+    if ps -p $PID &>/dev/null; then COUNT=$(($COUNT+1)); fi
+    if [ $COUNT -ge $JOB_LIMIT ]; then return 0; fi
+  done
+  return 1
+}
+
+function collect_running_pids() {
+  local INPUT_PIDS=("$@")
+  local RUNNING_PIDS=()
+  for PID in "${INPUT_PIDS[@]}"; do
+    if ps -p $PID &>/dev/null; then
+      RUNNING_PIDS+=("$PID")
+    fi
+  done
+  echo "${RUNNING_PIDS[@]}"
+}
+
+function parse_issues_from_log() {
+  local LOG_FILE=$1
+  local FILE_FILTER=${2:-}
+  local PATTERN="$FILE_FILTER:[0-9]\+:[0-9]\+"
+  local NUM_ERRS=$(grep -c "$PATTERN:\ error:\ " "$LOG_FILE")
+  local NUM_WARNS=$(grep -c "$PATTERN:\ warning:\ " "$LOG_FILE")
+  local NUM_NOTES=$(grep -c "$PATTERN:\ note:\ " "$LOG_FILE")
+  local NUM_TOTAL=$(($NUM_ERRS+$NUM_WARNS+$NUM_NOTES))
+  echo $NUM_TOTAL
+  if [ $NUM_TOTAL -gt 0 ]; then return 0; fi
+  return 1
+}
+
+function compute_hash() {
+  echo "$1" | sha1sum | cut -d' ' -f1
+}
+
+readonly VER_HASH="$(compute_hash "$(clang-tidy -version)")"
+readonly CFG_HASH="$(compute_hash "$(clang-tidy -dump-config)")"
+
+function exec_clang_tidy() {
+  local SRC_FILE="$1"
+  local NO_CACHE=${2:-false}
+  local JOB_PID=$BASHPID
+
+  local CACHE_ENTRY=
+
+  if ! $NO_CACHE; then
+    # compute hash from file content with all headers included
+    local FILE_CONTENT="$(clang -E ${CC_FLAGS[@]} "${SRC_FILE}" 2>/dev/null)"
+    local FILE_HASH="$(compute_hash "$FILE_CONTENT")"
+
+    # compute cache entry and exit on cache hit
+    CACHE_ENTRY="$CACHE_DIR/$(compute_hash "$VER_HASH+$CFG_HASH+$FILE_HASH")"
+    if [ -f "$CACHE_ENTRY" ]; then return; fi
+  fi
+
+  # exec clang-tidy and store log for this job
+  clang-tidy ${CC_FLAGS[@]/#/-extra-arg=} "$SRC_FILE" \
+             2>/dev/null >"$LOG_DIR/$JOB_PID" || true
+
+  # if issues are related to this file (and not only to its dependencies)
+  if parse_issues_from_log "$LOG_DIR/$JOB_PID" "$SRC_FILE" &>/dev/null; then
+    # record failure and store this job's pid (atomic create and rename dance)
+    echo $JOB_PID > "$FAIL_FLAG.$JOB_PID"
+    mv --no-clobber "$FAIL_FLAG.$JOB_PID" "$FAIL_FLAG"
+  elif [ -n "$CACHE_ENTRY" ]; then
+    # record success by creating cache entry and remove log
+    touch "$CACHE_ENTRY"
+    rm -f "$LOG_DIR/$JOB_PID"
+  fi
+}
+
+function run_clang_tidy() {
+  local SRC_DIR="$(pwd)"
+  local EXCL_REGX=
+  local NO_CACHE=false
+  local JOB_LIMIT=$(nproc --all)
+  local STOP_ON_FAILURE=false
+  local VERBOSE=false
+
+  # parse source directory argument
+  if [ -n "$1" ] && ! [[ "$1" =~ ^- ]]; then
+    SRC_DIR="$1"
+    shift
+  fi
+
+  # parse remaining arguments
+  while getopts "h?e:j:nsv" OPT "$@"; do
+    case "${OPT}" in
+      h|\?)
+        usage
+        exit 0
+        ;;
+      e)
+        EXCL_REGX="${OPTARG}"
+        ;;
+      j)
+        JOB_LIMIT="${OPTARG}"
+        ;;
+      n)
+        NO_CACHE=true
+        ;;
+      s)
+        STOP_ON_FAILURE=true
+        ;;
+      v)
+        VERBOSE=true
+        ;;
+    esac
+  done
+
+  mkdir -p "$LOG_DIR"
+  mkdir -p "$CACHE_DIR"
+
+  printf "Running clang-tidy on %s\n" "$SRC_DIR"
+
+  local RETVAL=0 CHILD_PIDS=() PID_FILE_MAP=() SRC_FILE
+
+  while read -r SRC_FILE; do
+    # skip non-C/C++ files
+    if ! file "$SRC_FILE" | grep -iq "c source\|c++ source"; then continue; fi
+
+    # skip on exclude regex match
+    if [ -n "$EXCL_REGX" ] && [[ "$SRC_FILE" =~ $EXCL_REGX ]]; then continue; fi
+
+    # adhere to job limit
+    while job_limit_reached $JOB_LIMIT "${CHILD_PIDS[@]}"; do
+      # wait for at least one job to finish
+      wait -fn ${CHILD_PIDS[@]}
+    done
+
+    # early exit on failure
+    if $STOP_ON_FAILURE && [ -f "$FAIL_FLAG" ]; then break; fi
+
+    # execute clang-tidy as background job
+    exec_clang_tidy "$SRC_FILE" $NO_CACHE &
+
+    # record new child pid
+    CHILD_PIDS=($(collect_running_pids "${CHILD_PIDS[@]}" "$!"))
+    PID_FILE_MAP[$!]="$SRC_FILE"
+  done < <(find "${SRC_DIR}" -type f)
+
+  # wait for all remaining children to finish
+  wait -f ${CHILD_PIDS[@]}
+
+  if [ -f "$FAIL_FLAG" ]; then
+    RETVAL=1
+
+    if $STOP_ON_FAILURE; then
+      # remove all but first failed pid's log
+      local FAIL_PID=$(cat "$FAIL_FLAG")
+      echo "$(find "$LOG_DIR" -type f | grep -v $FAIL_PID)" | xargs rm -f
+    fi
+
+    for PID in $(ls "$LOG_DIR"); do
+      # report issues from logs
+      local SRC_FILE="${PID_FILE_MAP[$PID]}"
+      local NUM_ISSUES=$(parse_issues_from_log "$LOG_DIR/$PID" "$SRC_FILE")
+      printf "%4d issue(s) in %s\n" $NUM_ISSUES "$SRC_FILE"
+      if $VERBOSE; then cat "$LOG_DIR/$PID"; fi
+    done
+  fi
+
+  # cleanup
+  rm -rf "$EXEC_DIR"
+
+  return $RETVAL
+}
+
+if [[ "$0" == "$BASH_SOURCE" ]]; then
+  run_clang_tidy "$@"
+  exit $?
+fi
