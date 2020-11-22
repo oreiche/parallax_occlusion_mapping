38a34924aa00c13d39dfc7adc0558c8b0844f14e
Oliver Reiche <oliver.reiche@gmail.com>
2020-11-22 13:07:50 +0100

    Externals: Make sha256 optional and support extract patterns

--- cmake/externals.cmake
+++ cmake/externals.cmake
@@ -15,28 +15,42 @@ macro(setup_externals)
 endmacro()
 
 # Create external from HTTP archive.
-# @param NAME    <string> Name of the external to create.
-# @param URL     <string> URL for downlading archive.
-# @param SHA256  <string> SHA256 hash of archive.
-# @param STRIP   <string> Prefix to strip archive path. (optional)
-# @param TIMEOUT <int>    Timeout for downloading archive. (optional)
-# @param VERBOSE          Show download progress. (optional)
+# @param NAME     <string>  Name of the external to create.
+# @param URL      <string>  URL for downlading archive.
+# @param SHA256   <string>  SHA256 hash of archive. (optional)
+# @param STRIP    <string>  Prefix to strip archive path. (optional)
+# @param PATTERNS <strings> Select files and directories to extract. (optional)
+# @param TIMEOUT  <int>     Timeout for downloading archive. (optional)
+# @param VERBOSE            Show download progress. (optional)
 # @return Sets variable ${NAME}_ROOT to root directory of unpacked archive.
 macro(http_archive)
     setup_externals()
 
     set(options VERBOSE)
     set(oneValueArgs NAME URL SHA256 STRIP TIMEOUT)
-    cmake_parse_arguments(HTTP_ARCHIVE "${options}" "${oneValueArgs}" "" ${ARGN})
+    set(multiValueArgs PATTERNS)
+    cmake_parse_arguments(HTTP_ARCHIVE
+         "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    if (NOT HTTP_ARCHIVE_NAME OR NOT HTTP_ARCHIVE_URL OR NOT HTTP_ARCHIVE_SHA256)
+    if (NOT HTTP_ARCHIVE_NAME OR NOT HTTP_ARCHIVE_URL)
         message(FATAL_ERROR "Missing mandatory argument")
     endif()
-    if (NOT HTTP_ARCHIVE_TIMEOUT)
-        set(HTTP_ARCHIVE_TIMEOUT 60)
+
+    if (HTTP_ARCHIVE_SHA256)
+        set(DOWNLOAD_EXTRA_ARGS ${DOWNLOAD_EXTRA_ARGS}
+            EXPECTED_HASH SHA256=${HTTP_ARCHIVE_SHA256})
+    endif()
+    if (HTTP_ARCHIVE_TIMEOUT)
+        set(DOWNLOAD_EXTRA_ARGS ${DOWNLOAD_EXTRA_ARGS}
+            TIMEOUT ${HTTP_ARCHIVE_TIMEOUT})
     endif()
     if (HTTP_ARCHIVE_VERBOSE)
-        set(DOWNLOAD_EXTRA_ARGS SHOW_PROGRESS)
+        set(DOWNLOAD_EXTRA_ARGS ${DOWNLOAD_EXTRA_ARGS}
+            SHOW_PROGRESS)
+    endif()
+    if (HTTP_ARCHIVE_PATTERNS)
+        set(EXTRACT_EXTRA_ARGS ${EXTRACT_EXTRA_ARGS}
+            PATTERNS ${HTTP_ARCHIVE_PATTERNS})
     endif()
 
     set(DOWNLOAD_FILE "${EXTERNALS_FETCH_DIR}/${HTTP_ARCHIVE_NAME}_archive")
@@ -48,20 +62,20 @@ macro(http_archive)
     file(DOWNLOAD "${HTTP_ARCHIVE_URL}"
         ${DOWNLOAD_FILE}
         ${DOWNLOAD_EXTRA_ARGS}
-        TIMEOUT ${HTTP_ARCHIVE_TIMEOUT}
-        EXPECTED_HASH SHA256=${HTTP_ARCHIVE_SHA256}
         STATUS RETVAL)
     list(GET RETVAL 0 EXIT_CODE)
     list(GET RETVAL 1 EXIT_MESSAGE)
 
     if (${EXIT_CODE} EQUAL 0)
         # Download call successful, file might have existed before
-        if ("${EXIT_MESSAGE}" STREQUAL "\"No error\"")
+        if ("${EXIT_MESSAGE}" STREQUAL "\"No error\""
+            OR NOT EXISTS "${STAGE_PATH}")
             # New file was successfully downloaded
-            message(STATUS "Extracting ${HTTP_ARCHIVE_NAME} to external")
+            message(STATUS "Extracting external ${HTTP_ARCHIVE_NAME}")
             file(ARCHIVE_EXTRACT
                 INPUT "${DOWNLOAD_FILE}"
-                DESTINATION "${EXTRACT_PATH}")
+                DESTINATION "${EXTRACT_PATH}"
+                ${EXTRACT_EXTRA_ARGS})
             file(REMOVE_RECURSE "${STAGE_PATH}")
             file(RENAME "${EXTRACT_PATH}/${HTTP_ARCHIVE_STRIP}"
                         "${STAGE_PATH}")
