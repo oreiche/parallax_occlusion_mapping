d3e0fba3aef57055f0ed56a122b52c46adb98b48
Oliver Reiche <oliver.reiche@gmail.com>
2020-12-10 20:44:49 +0100

    Add camera and movement handling to renderer

--- src/pom/renderer/renderer.hpp
+++ src/pom/renderer/renderer.hpp
@@ -6,6 +6,7 @@
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 #include "gsl/gsl-lite.hpp"
+#include "src/pom/renderer/camera.hpp"
 
 class Renderer {
  public:
@@ -24,15 +25,21 @@ class Renderer {
 
   [[nodiscard]] auto Run(DrawCallback const& draw_call) noexcept -> bool;
 
+  [[nodiscard]] auto GetViewMatrix() const noexcept -> glm::mat4 {
+    return camera_.GetViewMatrix();
+  }
+
  private:
   static constexpr auto kWidth = 1024;
   static constexpr auto kHeight = 768;
   bool initialized_{};
   std::string name_{};
   GLFWwindow* window_{};
+  Camera camera_{};
   int exit_keycode_{GLFW_KEY_ESCAPE};
 
   [[nodiscard]] auto Initialize() noexcept -> bool;
+  [[nodiscard]] auto HandleKey() noexcept -> bool;
 };
 
 #endif  // SRC_POM_RENDERER_RENDERER_HPP
